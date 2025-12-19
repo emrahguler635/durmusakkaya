56a05fc239b1eb74df6ba04f381321fe531c2e76
Emrah <emrahguler52@hotmail.com>
2025-12-20 01:30:59 +0300

    Create admin-data.ts
    Add GitHub auto-commit and deploy feature

---
+++ lib/admin-data.ts
@@ -0,0 +1,9 @@
+// Auto-generated data file - DO NOT EDIT MANUALLY
+// This file is generated from admin panel changes
+// If this file doesn't exist or values are null, pages will use static data
+
+export const adminHomeData: any = null;
+export const adminAboutData: any = null;
+export const adminContactData: any = null;
+export const adminNewsData: any[] | null = null;
+
