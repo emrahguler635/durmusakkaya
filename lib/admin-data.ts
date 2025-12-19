598db3fe8d78a805cc75169c30c7b5521f055376
Emrah Guler <emrahguler52@hotmail.com>
2025-12-19 09:36:30 +0300

    Remove admin-data.ts file to fix build

--- lib/admin-data.ts
+++
@@ -1,9 +0,0 @@
-// Auto-generated data file - DO NOT EDIT MANUALLY
-// This file is generated from admin panel changes
-// If this file doesn't exist, pages will use static data
-
-export const adminHomeData = null;
-export const adminAboutData = null;
-export const adminContactData = null;
-export const adminNewsData = null;
-
